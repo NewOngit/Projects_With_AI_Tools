9a8436d7b5245f1d454b14999d1f39f85b7a5ef8
new <uday45855@gmail.com>
2024-05-16 01:04:37 -0700

    Commited the first Py file using git

---
+++ Python_Project/Solar_PhotoVoltaic_Generation_Prediction_And_Evaluation_With_ML_DL_Hybrid_Models.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
+from sklearn.metrics import mean_squared_error,mean_absolute_error,mean_absolute_percentage_error
+from math import sqrt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from numpy import concatenate
+from statsmodels.tsa.seasonal import seasonal_decompose
+from tensorflow.keras.layers import InputLayer,LSTM,Dense,Conv1D, MaxPooling1D, Flatten, Dense, Input,Dropout
+from tensorflow.keras.models import Sequential
+
+###Fetching the file 
+#df_ALL=pd.read_excel("Weather_Data_14-11-30.xlsx")
+df_ALL=pd.read_excel("https://data.london.gov.uk/download/photovoltaic--pv--solar-panel-energy-generation-data/b4a7e790-8cb8-451c-b828-c4c5d8445705/Weather%20Data%202014-11-30.xlsx")
+
+###Columns 
+df_ALL.columns
+########Collecting the only YMCA Sites Dataset#############
+df_YMCA=df_ALL[df_ALL['Site']=="YMCA"]
+##Dropping the columns
+
+ls=['Site',  'Month', 'Time', 'Hour','WindDir', 'WindRun', 'HiSpeed','HiDir','Bar','Rain', 'RainRate','InEMC',
+       'InAirDensity', 'ET', 'WindSamp', 'WindTx', 'ISSRecept', 'ArcInt']
+df_YMCA_M=df_YMCA.drop(ls,axis=1)
+
+SolarEnergy_Data=df_YMCA_M.loc[:,'SolarEnergy']
+
+SolarEnergy=SolarEnergy_Data.values.reshape(SolarEnergy_Data.values.shape[0],1)
+
+new_df_YMCA_M=df_YMCA_M.drop(['SolarEnergy'],axis=1)
+
+ls1=new_df_YMCA_M.columns
+
+df_lon=pd.DataFrame(np.concatenate((new_df_YMCA_M,SolarEnergy),axis=1))
+
+New_Cols=(ls1).to_list()
+
+New_Cols.append("SolarEnergy")
+len(New_Cols)
+
+df_lon.columns=New_Cols
+
+#df_lon=pd.read_csv("D:/new_Major_Final.csv",index_col=0)
+#df_lon=pd.read_csv("D:/new_Major.csv",index_col=0)
+
+df_lon.head()
+
+#df_lon.describe(include='all',datetime_is_numeric=True) 
+
+(df_lon['THSWIndex']=='---').sum()
+
+col_=df_lon.columns
+col_.size
+
+###################################Function definition for feature selection and change the proper datatypes##################
+def feature_Selection(Dataframe,col_):  
+    for _ in col_:
+        if (Dataframe[_]=='---').sum()<len(Dataframe[_])/2:
+            df_=Dataframe.replace({_: r'^--.$'}, {_: np.NaN}, regex=True)
+        else:
+            Dataframe=Dataframe.drop([_],axis=1)
+    return df_
+
+df_=feature_Selection(df_lon,df_lon.columns)
+df_
+
+print("The NaN elements are :")
+print(df_lon.isna().sum())
+explodes=(0,0.15)
+plt.figure(0)
+if df_lon.isna().sum().sum()!=0:
+   plt.pie(df_lon.isna().value_counts(),explode=explodes,startangle=0,colors=["firebrick","indianred"],
+    labels=['Non NaN elements',str("%0.2f"%(df_lon.isnull().sum().sum()*100/len(df_lon.Wind_Speed)))+'% NaN elements'],textprops={'fontsize':'20'}) 
+else :
+    plt.pie(df_lon.isna().value_counts(),startangle=0,colors=["firebrick","indianred"],
+    labels=['There is no NaN element present'],textprops={'fontsize':'20'}) 
+
+    
+#Dropping the inappropriate type columns from the dataset
+df_=df_.drop(['Date'],axis=1)
+
+
